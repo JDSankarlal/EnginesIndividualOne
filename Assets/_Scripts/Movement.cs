8ac2b8a3f934a4c11c9ed8fe3841be2a66cfab76
Josh Sankarlal <joshua.sankarlal@uoit.net>
2019-09-25 16:22:13 -0400

    add movement script and blank sendtransform script

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        } 
+
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime);
+        }    
+
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+    }
+}
