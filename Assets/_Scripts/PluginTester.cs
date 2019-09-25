5e88b8781292728c7c12f3d1a43931a2348d0b4a
Josh Sankarlal <joshua.sankarlal@uoit.net>
2019-09-25 16:35:46 -0400

    Add Plugin tester

---
+++ Assets/_Scripts/PluginTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class PluginTester : MonoBehaviour
+{
+    const string DLL_NAME = "Tutorial2";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(SimpleFunction());
+        }
+    }
+}
