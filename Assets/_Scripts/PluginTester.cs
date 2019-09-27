31d34f5ceba34fec15fe661605eae79d1fde4f4d
Josh Sankarlal <joshua.sankarlal@uoit.net>
2019-09-27 02:21:52 -0400

    Thanks Unity for fucking around and keeping the same DLL loaded for the past 3 hours of my life

--- Assets/_Scripts/PluginTester.cs
+++ Assets/_Scripts/PluginTester.cs
@@ -7,21 +7,23 @@ using System.Runtime.InteropServices;
 public class PluginTester : MonoBehaviour
 {
     const string DLL_NAME = "Tutorial2";
-    [DllImport(DLL_NAME)]
-    private static extern int SimpleFunction();
-    [DllImport(DLL_NAME)]
-    private static extern float GetTransform(float x, float y, float z);
+
+   [DllImport(DLL_NAME, EntryPoint="SimpleFunction")]
+   private static extern int SimpleFunction();
+
+    [DllImport(DLL_NAME,EntryPoint="GetUnityTransformX")]
+    private static extern float GetUnityTransformX();
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Debug.Log(SimpleFunction());
+           Debug.Log(SimpleFunction());
         }
 
         if (Input.GetKeyDown(KeyCode.K))
         {
-            Debug.Log(GetTransform(transform.position.x, transform.position.y, transform.position.z));
+            Debug.Log(GetUnityTransformX());
            
         }
     }
