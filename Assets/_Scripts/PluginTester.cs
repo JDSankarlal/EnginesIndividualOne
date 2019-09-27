240010d45813850abdba9cc596fedd625fd5b79d
Josh Sankarlal <joshua.sankarlal@uoit.net>
2019-09-26 23:50:08 -0400

    Revert "Add GetTransform function to extern c in wrapper"
    This reverts commit daae77434cc4045f6328babe68e5de7a8ec32ec0.

---
+++ Assets/_Scripts/PluginTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class PluginTester : MonoBehaviour
+{
+    const string DLL_NAME = "Tutorial2";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+    [DllImport(DLL_NAME)]
+    private static extern float GetTransform(float x, float y, float z);
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(SimpleFunction());
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log(GetTransform(transform.position.x, transform.position.y, transform.position.z));
+           
+        }
+    }
+}
